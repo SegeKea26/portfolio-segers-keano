c2bb628ca496f35bf4663b327f4373e8a913f1bb
SegeKea26 <keano.segers@student.arteveldehs.be>
2025-05-22 08:35:04 +0200

    Updated webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,7 +4,6 @@ mix
    .js('src/scripts/main.js', 'dist/scripts/')
    .setPublicPath('dist')
    .postCss('src/styles/main.css', 'dist/styles/')
-   .copy('src/assets/fonts/froles.otf', 'dist/fonts/')
 
    .browserSync({
       server: true
